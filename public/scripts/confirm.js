1420c8b06012df3fbfdc1fb72d8e4ed09e54ef86
vagrant <kennethracadio@hotmail.com>
2018-02-11 22:15:31 +0000

    Clean up code

--- public/scripts/confirm.js
+++ public/scripts/confirm.js
@@ -1,28 +1,20 @@
 
 $( document ).ready(function() {
 
-  console.log('hey')
-
   function updatePost() {
       setInterval(function() {
-
+          // grab url id
           const url = window.location.pathname;
           const id = url.substring(url.lastIndexOf('/') + 1);
-          console.log(id);
 
           $.ajax({
               type: "GET",
               url: `/query/${id}`, //
               success: function(response) {
-                console.log('response', response);
                 const status = response[0].status;
                 const eta = response[0].eta;
-
-                console.log('status', status);
-                console.log('etaaa', eta);
                   // If not false, update the page
                 if (status === 'confirmed') {
-                  console.log('here')
                   $("h3").text(`Your order will be ready for pick up in ${eta} minutes.`);
                 }
               }
